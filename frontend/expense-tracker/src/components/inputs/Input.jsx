de632cae492139609c72681039a8d298c4f580d8
ImalkaDinithi <imalkadinithi2003@gmail.com>
2026-04-13 11:55:21 +0530

    Initial commit

---
+++ frontend/expense-tracker/src/components/inputs/Input.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa6";
+
+
+const Input = ({ value, onChange, placeholder, label, type = "text" }) => {
+    const [showpassword, setShowpassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowpassword(!showpassword);
+    };
+    return (
+        <div>
+            <label className="text-[13px] text-slate-800">{label}</label>
+
+            <div className="input-box">
+                <input
+                    type={type === "password" ? (showpassword ? "text" : "password") : type}
+                    placeholder={placeholder}
+                    className="w-full bg-transparent outline-none"
+                    value={value}
+                    onChange={(e) => onChange(e)}
+                />
+
+                {type === "password" && (
+                    <>
+                        {showpassword ? (
+                            <FaRegEye
+                                size={22}
+                                className="text-primary cursor-pointer"
+                                onClick={() => togglePasswordVisibility()}
+                            />
+                        ) : (
+                            <FaRegEyeSlash
+                                size={22}
+                                className="text-slate-400 cursor-pointer"
+                                onClick={() => togglePasswordVisibility()}
+                            />
+                        )}
+                    </>
+                )}
+            </div>
+        </div >
+    )
+}
+
+export default Input;
