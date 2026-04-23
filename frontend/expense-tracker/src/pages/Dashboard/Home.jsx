786adf27bd1c3a56c26cba9c5a1790b1a912adf5
ImalkaDinithi <imalkadinithi2003@gmail.com>
2026-04-23 21:34:24 +0530

    Create the Expense Details Section

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -11,6 +11,7 @@ import { IoMdCard } from "react-icons/io";
 import { addThousandSeparator } from "../../utils/helper";
 import RecentTransactions from "../../components/Dashboard/RecentTransactions";
 import FinanceOverview from "../../components/Dashboard/FinanceOverview";
+import ExpenseTransactions from "../../components/Dashboard/ExpenseTransactions";
 
 const Home = () => {
     useUserAuth();
@@ -72,7 +73,7 @@ const Home = () => {
                     </div> */}
 
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
-                    <RecentTransactions
+                    {/* <RecentTransactions
                         transactions={dashboardData?.recentTransactions}
                         onSeeMore={() => navigate("/expense")}
                     />
@@ -81,7 +82,14 @@ const Home = () => {
                         totalBalance={dashboardData?.totalBalance || 0}
                         totalIncome={dashboardData?.totalIncome || 0}
                         totalExpense={dashboardData?.totalExpenses || 0}
+                    /> */}
+
+                    <ExpenseTransactions
+                        transactions={dashboardData?.last30DaysExpenses?.transactions || []}
+                        onSeeMore={() => navigate("/expense")}
                     />
+
+                    
                 </div>
             </div>
         </DashboardLayout>
