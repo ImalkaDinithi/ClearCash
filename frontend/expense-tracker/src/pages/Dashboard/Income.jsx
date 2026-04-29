88f3184666ca94e9a570745de36553cf70e999ca
ImalkaDinithi <imalkadinithi2003@gmail.com>
2026-04-29 19:49:15 +0530

    API Call to Delete Income

--- frontend/expense-tracker/src/pages/Dashboard/Income.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Income.jsx
@@ -8,8 +8,10 @@ import AddIncomeForm from "../../components/Income/AddIncomeForm";
 import { toast } from "react-hot-toast";
 import IncomeList from "../../components/Income/IncomeList";
 import DeleteAlert from "../../components/DeleteAlert"
-
+import { useUserAuth } from "../../hooks/useUserAuth";
+    
 const Income = () => {
+    useUserAuth();
 
     const [incomeData, setIncomeData] = useState([]);
     const [loading, setLoading] = useState(false);
@@ -80,7 +82,20 @@ const Income = () => {
     };
 
     // Delete Income
-    const deleteIncome = async (id) => { };
+    const deleteIncome = async (id) => {
+        try {
+            await axiosInstance.delete(API_PATHS.INCOME.DELETE_INCOME(id));
+
+            setOpenDeleteAlert({ show: false, data: null });
+            toast.success("Inocme details deleted successfully");
+            fetchIncomeDetails();
+        } catch (error) {
+            console.error(
+                "Error deleting income:",
+                error.response?.data?.message || error.message
+            );
+        }
+    };
 
     // handle download income details
     const handleDownloadIncomeDetails = async () => { };
@@ -125,9 +140,9 @@ const Income = () => {
                     onClose={() => setOpenDeleteAlert({ show: false, data: null })}
                     title="Delete Income"
                 >
-                    <DeleteAlert 
-                    content="Are you sure you want to delete this income detail?"
-                    onDelete={() => deleteInocme(openDeleteAlert.data)}
+                    <DeleteAlert
+                        content="Are you sure you want to delete this income detail?"
+                        onDelete={() => deleteIncome(openDeleteAlert.data.id)}
                     />
                 </Modal>
             </div>
