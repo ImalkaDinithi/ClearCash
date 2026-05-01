d61fb1e6572cd09608e0558d5b769b1f89736d2d
ImalkaDinithi <imalkadinithi2003@gmail.com>
2026-05-01 22:50:16 +0530

    Implement Expense List Section

--- frontend/expense-tracker/src/pages/Dashboard/Expense.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Expense.jsx
@@ -8,6 +8,8 @@ import ExpenseOverview from "../../components/Expense/ExpenseOverview";
 import Modal from "../../components/Modal";
 import AddExpenseForm from "../../components/Expense/AddExpenseForm";
 import ExpenseList from "../../components/Expense/ExpenseList";
+import { toast } from "react-hot-toast";
+import DeleteAlert from "../../components/DeleteAlert"
 
 const Expense = () => {
     useUserAuth();
@@ -132,6 +134,17 @@ const Expense = () => {
                     title="Add Expense"
                 >
                     <AddExpenseForm onAddExpense={handleAddExpense} />
+                </Modal>
+
+                <Modal
+                    isOpen={openDeleteAlert.show}
+                    onClose={() => setOpenDeleteAlert({ show: false, data: null })}
+                    title="Delete Expense"
+                >
+                    <DeleteAlert
+                        content="Are you sure you want to delete this expense detail?"
+                        onDelete={() => deleteExpense(openDeleteAlert.data)}
+                    />
                 </Modal>
 
             </div>
